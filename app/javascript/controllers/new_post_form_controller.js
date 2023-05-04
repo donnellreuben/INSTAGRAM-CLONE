be5997324d753b9a361a2143564b3f7752838944
Donnell Reuben <donnell.reuben@icloud.com>
2023-05-04 18:12:00 -0500

    installed cropper, yarn and node

--- app/javascript/controllers/new_post_form_controller.js
+++ app/javascript/controllers/new_post_form_controller.js
@@ -1,10 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
+import Cropper from 'cropperjs';
 
 // Connects to data-controller="new-post-form"
 export default class extends Controller {
   static targets = ['selectFileButton', 'photoFile'];
 
   connect() {
+    console.log('hello world from new post form controller');
+    console.log('Cropper: ' + Cropper);
   }
 
   selectFile(e) {
